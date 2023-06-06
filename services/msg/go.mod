a3b86dc6df3f6fb0f961e86ff79d657f72d427b9
Semyon981 <semenpermogorov@gmail.com>
2023-06-07 00:19:48 +0300

    Add

--- services/msg/go.mod
+++ services/msg/go.mod
@@ -2,6 +2,17 @@ module github.com/Semyon981/nexus/services/msg
 
 go 1.20
 
-require github.com/jmoiron/sqlx v1.3.5
+require (
+	github.com/Semyon981/nexus/proto v0.0.0-20230606202419-1353f89f1a51
+	github.com/jmoiron/sqlx v1.3.5
+)
 
-require github.com/Semyon981/nexus/proto v0.0.0-20230606202419-1353f89f1a51 // indirect
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- services/msg/service/server/server.go
+++ services/msg/service/server/server.go
@@ -1,19 +1,46 @@
 package server
 
 import (
+	"context"
+	"time"
+
+	"github.com/Semyon981/nexus/proto/msgpb"
+	"github.com/Semyon981/nexus/proto/userspb"
+	"github.com/Semyon981/nexus/services/msg/models"
 	"github.com/Semyon981/nexus/services/msg/service"
 	"github.com/Semyon981/nexus/services/msg/service/repository/postgresql"
 	"github.com/jmoiron/sqlx"
 )
 
 type server struct {
-	//msgpb.UnimplementedUserServiceServer
-
-	repo service.Repository
+	msgpb.UnimplementedUserServiceServer
+	repo        service.Repository
+	usersclient userspb.UserServiceClient
 }
 
 func NewServer(db *sqlx.DB) *server {
 	return &server{
 		repo: *postgresql.NewUserRepository(db),
 	}
+}
+
+func (s *server) GetMessages(ctx context.Context, in *msgpb.GetMessagesRequest) (*msgpb.GetMessagesResponse, error) {
+	res := make([]models.Message, 0)
+
+}
+
+func (s *server) SendMessage(ctx context.Context, in *msgpb.SendMessageRequest) (*msgpb.SendMessageResponse, error) {
+	_, err := s.usersclient.GetUserId(ctx, &userspb.GetUserIdRequest{IdUsers: in.IdTo})
+
+	if err != nil {
+		return &msgpb.SendMessageResponse{}, err
+	}
+
+	err = s.repo.SendMessage(ctx, in.IdFrom, in.IdTo, in.Msg, time.Unix(in.Time, 0))
+	if err != nil {
+		return &msgpb.SendMessageResponse{}, err
+	}
+
+	return &msgpb.SendMessageResponse{}, nil
+
 }
