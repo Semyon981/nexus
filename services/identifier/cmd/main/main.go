dd8b4ae811d8051de8642174790cf42665829470
Semyon981 <semenpermogorov@gmail.com>
2023-06-06 20:39:02 +0300

    hjk

--- services/identifier/cmd/main/main.go
+++ services/identifier/cmd/main/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/Semyon981/nexus/proto/identifierpb"
+	"github.com/Semyon981/nexus/services/identifier/config"
+	"github.com/Semyon981/nexus/services/identifier/service/server"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	if err := config.Init(); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	srv := server.NewServer(viper.GetString("auth.hash_salt"), []byte(viper.GetString("auth.signing_key")))
+
+	s := grpc.NewServer()
+
+	identifierpb.RegisterServiceServer(s, srv)
+
+	lis, err := net.Listen("tcp", ":"+viper.GetString("port"))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- services/identifier/service/server/server.go
+++ services/identifier/service/server/server.go
@@ -1,7 +1,68 @@
 package server
 
-import "github.com/Semyon981/nexus/proto/identifierpb"
+import (
+	"context"
+	"crypto/sha1"
+	"fmt"
+	"time"
+
+	"github.com/Semyon981/nexus/proto/identifierpb"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type server struct {
 	identifierpb.UnimplementedServiceServer
+	hashSalt   string
+	signingKey []byte
+}
+
+func NewServer(hashSalt string, signingKey []byte) *server {
+	return &server{hashSalt: hashSalt, signingKey: signingKey}
+}
+
+func (s *server) Hash(ctx context.Context, in *identifierpb.HashRequest) (*identifierpb.HashResponse, error) {
+	pwd := sha1.New()
+	pwd.Write([]byte(in.Str))
+	pwd.Write([]byte(s.hashSalt))
+
+	return &identifierpb.HashResponse{Hash: fmt.Sprintf("%x", pwd.Sum(nil))}, nil
+}
+
+type AuthClaims struct {
+	jwt.RegisteredClaims
+}
+
+func (s *server) JwtGen(ctx context.Context, in *identifierpb.JwtGenRequest) (*identifierpb.JwtGenResponse, error) {
+
+	claims := AuthClaims{
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Second * time.Duration(in.Time))),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   in.Subject,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tok, err := token.SignedString(s.signingKey)
+	return &identifierpb.JwtGenResponse{Token: tok}, err
+}
+
+func (s *server) JwtParse(ctx context.Context, in *identifierpb.JwtParseRequest) (*identifierpb.JwtParseResponse, error) {
+	token, err := jwt.ParseWithClaims(in.Token, &AuthClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return s.signingKey, nil
+	})
+
+	if err != nil {
+		return &identifierpb.JwtParseResponse{}, err
+	}
+
+	if claims, ok := token.Claims.(*AuthClaims); ok && token.Valid {
+
+		return &identifierpb.JwtParseResponse{Subject: claims.Subject}, nil
+	}
+
+	return &identifierpb.JwtParseResponse{}, fmt.Errorf("invalid access token")
 }
